a1f4ea3b7cc0d27822134990d7ddf5a0a768e789
fatherofikura <fatherofikura0107@gmail.com>
2019-07-10 07:32:09 +0900

    削除functionのデバッグ中

--- functions/index.js
+++ functions/index.js
@@ -83,7 +83,7 @@ exports.deleteMember = functions.https.onCall( (data, context) => {
   }
 
   //
-  return admin.database().ref('/member/' + uid).removeValue().then(snapshot => {
+  return admin.database().ref('/member/' + uid).remove().then(snapshot => {
     console.log('value', snapshot.val());
     return snapshot.val();
   }).catch((error) => {
