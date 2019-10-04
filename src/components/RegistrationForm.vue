93095cb9b2e19a39f3a4d043370eb8dd0fc3b573
fatherofikura <fatherofikura0107@gmail.com>
2019-10-04 11:41:10 +0900

    PhoneNumberの対応(半角の数字のみ)

--- src/components/RegistrationForm.vue
+++ src/components/RegistrationForm.vue
@@ -62,3 +62,18 @@ export default {
   }
 };
 </script>
+
+<style>
+input[type="number"]::-webkit-outer-spin-button,
+input[type="number"]::-webkit-inner-spin-button {
+     -webkit-appearance: none;
+     margin: 0;
+}
+
+input[type="number"] {
+     -moz-appearance:textfield;
+}
+input::-ms-clear {
+     visibility:hidden
+}
+</style>
